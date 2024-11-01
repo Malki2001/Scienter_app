d98fc99fdae6c5d31e8205fd9fb027fdd22397f4
Malki2001 <malkiyasodhara18@gmail.com>
2024-11-02 00:31:39 +0530

    Begin , validate and structure of pages

---
+++ scienter_task/lib/app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:scienter_task/screens/welcome_screen.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+        fontFamily: 'Lato',
+      ),
+      home: const WelcomeScreen(),
+    );
+  }
+}
